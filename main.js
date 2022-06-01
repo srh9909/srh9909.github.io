1e498a7b473976dab66287a51f149d0539c9348a
Rebecca Hwang <rebeccahwang@outlook.com.au>
2022-06-01 17:00:14 +1000

    homepage + images

---
+++ main.js
@@ -0,0 +1,8 @@
+function buttonClick(buttonElement){
+    alert("Hey you clicked " + buttonElement.textContent);
+    console.log("Hey you clicked " + buttonElement.textContent);
+}
+
+window.addEventListener("DOMContentLoaded" function() {
+
+});
