f7163fffcc41cf77d9e7ba935f96be8f32fe7adc
Rebecca Hwang <rebeccahwang@outlook.com.au>
2022-06-10 17:25:58 +1000

    updating links to docs

--- main.js
+++
@@ -1,8 +0,0 @@
-function buttonClick(buttonElement){
-    alert("Hey you clicked " + buttonElement.textContent);
-    console.log("Hey you clicked " + buttonElement.textContent);
-}
-
-window.addEventListener("DOMContentLoaded" function() {
-
-});
